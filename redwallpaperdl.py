72144187e0c301aa601be089cc9f2d6da1c56a44
shankar <123456789987654321sr@gmail.com>
2021-08-15 18:28:04 +0530

    created script for downloading wallpaper from reddit and saving it to fs

---
+++ redwallpaperdl.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# This script downloads the most trending 10 wallpapers from a subreddit and saves it to a folder.
+"""
+Copyright (c) 2021 ShankarCodes. All rights reserved.
+
+You may use, distribute and modify this code under the terms of the 
+BSD 3-Clause "New" or "Revised" License.
+You should have received a copy of the BSD-3-Clause License with
+this file. If not visit https://opensource.org/licenses/BSD-3-Clause
+
+Homepage: https://github.com/ShankarCodes/scripts
+"""
+
+# Make sure to create a .env file which contains
+# ID=<your app id>
+# SECRET=<your app secret>
+# NAME=<your username>
+# PASSWORD=<your reddit password>
+
+# List of sum subreddits for wallpapers.
+# Remove or add your own subreddits here
+subreddits = [
+    'SpacePorn',
+    'EarthPorn',
+    'wallpaper'
+    'wallpapers',
+    'BotanicalPorn',
+    'CityPorn',
+    'WeatherPorn',
+    'SkyPorn',
+    'LakePorn',
+    'VillagePorn',
+    'BeachPorn',
+    'WaterPorn',
+]
+subreddits = [
+    'EarthPorn'
+]
